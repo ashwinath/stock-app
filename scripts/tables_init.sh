635d4f4120bdcdb33def66d4067630ed8953e734
ashwin <ashwinath@hotmail.com>
2017-12-14 16:30:04 +0800

    changed user/pass and insert scripts into db

--- scripts/tables_init.sh
+++ scripts/tables_init.sh
@@ -1,5 +1,5 @@
-psql -f sql/create_database.sql -U postgres;
-psql -f sql/quartz_scheduler.sql -U postgres;
-psql -f sql/tables.sql -U postgres;
-psql -f sql/dl_jobs.sql -U postgres;
-psql -f sql/system_config.sql -U postgres;
+psql -f sql/create_database.sql -U postgres -d stock_app;
+psql -f sql/quartz_scheduler.sql -U postgres -d stock_app;
+psql -f sql/tables.sql -U postgres -d stock_app;
+psql -f sql/dl_jobs.sql -U postgres -d stock_app;
+psql -f sql/system_config.sql -U postgres -d stock_app;
